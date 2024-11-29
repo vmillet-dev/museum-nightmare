cd53a5e3953cbea7bc1cf4e79819f4f1f4b9f5d3
Valentin MILLET <34792012+vmillet-dev@users.noreply.github.com>
2024-11-29 09:29:52 +0100

    Update dependencies.cmake

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -4,8 +4,8 @@ include(FetchContent)
 set(BOX2D_VERSION "3.0.0")
 set(SFML_VERSION "2.6.2")
 set(TOMLPLUSPLUS_VERSION "3.4.0")
-set(SPDLOG_VERSION "1.15.0")
-set(TMXLITE_VERSION "master")  # Using master since no version tag is available
+set(SPDLOG_VERSION "1.15.0"
+set(TMXLITE_VERSION "master")
 
 # Define required libraries
 set(SFML_LIBRARIES
@@ -57,24 +57,9 @@ FetchContent_Declare(
 FetchContent_Declare(
     tmxlite
     GIT_REPOSITORY https://github.com/fallahn/tmxlite.git
-    GIT_TAG        ${TMXLITE_VERSION}
+    GIT_TAG ${TMXLITE_VERSION}
+    SOURCE_SUBDIR tmxlite
 )
-
-# Configure tmxlite build options
-set(TMXLITE_STATIC_LIB TRUE CACHE BOOL "Build tmxlite as a static library" FORCE)
-set(TMXLITE_BUILD_EXAMPLES FALSE CACHE BOOL "Don't build tmxlite examples" FORCE)
-set(TMXLITE_BUILD_TESTS FALSE CACHE BOOL "Don't build tmxlite tests" FORCE)
-
-FetchContent_GetProperties(tmxlite)
-if(NOT tmxlite_POPULATED)
-    FetchContent_Populate(tmxlite)
-    add_subdirectory(${tmxlite_SOURCE_DIR}/tmxlite ${tmxlite_BINARY_DIR})
-endif()
-
-# Make sure tmxlite is available for linking
-if(NOT TARGET tmxlite)
-    message(FATAL_ERROR "tmxlite target not created!")
-endif()
 
 FetchContent_MakeAvailable(SFML tomlplusplus spdlog box2d tmxlite)
 add_library(box2d::box2d ALIAS box2d)
