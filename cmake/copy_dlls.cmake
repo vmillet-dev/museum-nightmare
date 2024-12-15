9780abdc1280e92966efde92e6b5ed4af56a92c7
Valentin MILLET <valentin.millet39@gmail.com>
2024-12-16 00:20:12 +0100

    [WiP] Add artefact upload and download on CI

--- cmake/copy_dlls.cmake
+++ cmake/copy_dlls.cmake
@@ -3,6 +3,7 @@ file(GLOB_RECURSE DLL_FILES_2 "${CMAKE_BINARY_DIR}/_deps/**/**/${CMAKE_BUILD_TYP
 file(GLOB_RECURSE DLL_FILES_3 "${CMAKE_BINARY_DIR}/_deps/**/extlibs/bin/x64/*.dll")
 set(ALL_FILES ${DLL_FILES_1} ${DLL_FILES_2} ${DLL_FILES_3})
 
-foreach(dll_file ${ALL_FILES})
-    file(COPY "${dll_file}" DESTINATION "${CMAKE_BINARY_DIR}")
+foreach(DLL_FILE ${ALL_FILES})
+    message(STATUS "Copying DLL: ${DLL_FILE}")
+    file(COPY "${DLL_FILE}" DESTINATION "${CMAKE_BINARY_DIR}")
 endforeach()
--- cmake/copy_dlls_for_packaging.cmake
+++ cmake/copy_dlls_for_packaging.cmake
@@ -1,8 +1,9 @@
-file(GLOB_RECURSE DLL_FILES_1 "${CMAKE_BINARY_DIR}/_deps/**/Release/*.dll")
-file(GLOB_RECURSE DLL_FILES_2 "${CMAKE_BINARY_DIR}/_deps/**/**/Release/*.dll")
+file(GLOB_RECURSE DLL_FILES_1 "${CMAKE_BINARY_DIR}/_deps/**/${CMAKE_BUILD_TYPE}/*.dll")
+file(GLOB_RECURSE DLL_FILES_2 "${CMAKE_BINARY_DIR}/_deps/**/**/${CMAKE_BUILD_TYPE}/*.dll")
 file(GLOB_RECURSE DLL_FILES_3 "${CMAKE_BINARY_DIR}/_deps/**/extlibs/bin/x64/*.dll")
 set(ALL_FILES ${DLL_FILES_1} ${DLL_FILES_2} ${DLL_FILES_3})
 
 foreach(DLL_FILE ${ALL_FILES})
+    message(STATUS "Copying DLL for packaging: ${DLL_FILE}")
 	file(COPY "${DLL_FILE}" DESTINATION "${CMAKE_BINARY_DIR}/Packaging/temp/")
 endforeach()
